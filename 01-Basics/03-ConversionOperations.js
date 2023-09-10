6a4505fcdb9fab52a9c9998f91eb5480ee25729f
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-10 09:12:01 +0000

    commit

--- 01-Basics/03-ConversionOperations.js
+++ 01-Basics/03-ConversionOperations.js
@@ -1,4 +1,35 @@
-let score = "33"
+//let score = null
+//let score = undefined
+let score = "abc"
+//console.log(typeof score);
+//console.log(typeof (score));
 
-console.log(typeof score);
-console.log(typeof (score));
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
+
+// "33" => 33
+//"33abc" =>  NaN (not a number)
+// true => 1; false => 0;
+
+
+
+//let isLoggedIn = 1
+//let isLoggedIn =""
+let isLoggedIn ="shan"
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); 
+
+
+// if we write 1 => true
+// if we write 0 => false "" => fals
+// "value" ture
+
+
+let someNumber = 33
+
+let stringNumber =  String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
