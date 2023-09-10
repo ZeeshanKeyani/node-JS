e88d0034eb8385a6ec9a26b9a79b1c88a63271f1
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-10 08:27:42 +0000

    code comit

---
+++ 01-Basics/02_Datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as a newer version 
+//new standard  
+
+//alert ("3 + 3") we are using node js not browser  
+console.log(3 +
+    3)
+console.log("zeeshan")
+
+
+
+let name = "zeeshan" //string data type
+let age = 18
+let isLoggedIn = true
+
+
+// number => 2 power 53
+//bigint
+//strin => ""
+//boolean  => true/fals
+
+//null  => stanalone value is representaion of an empty value
+//undefined => value not defined
+//symbol => uniqueness
+
+
+//objects
+console.log(typeof "highes")
