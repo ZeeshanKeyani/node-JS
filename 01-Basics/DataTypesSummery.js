212c8db572100ebb207042e9ab5bf51c7954eec9
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-11 19:06:43 +0000

    Datatypesummery

---
+++ 01-Basics/DataTypesSummery.js
@@ -0,0 +1,56 @@
+//Data types
+//primitive    			non primitive /refrerencetype
+//call by value			call by reference
+
+//Primitives(copy main values save hoti hain) 7 hain : String , Number, Boolean, null, undefined,
+// Symbol(to make a value unique), BigInt larger values
+// dynamic or statically typed language?
+
+//
+
+//Reference type/ Non Premitive direct allocation of the reference to the memory
+// Arrays, Objects,  functions   _to master javascript master objects, and webevents\\
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+
+let UserEmail; //undefined
+
+const id = Symbol('123')
+const anOtherId = Symbol('123')
+
+console.log( id === anOtherId);
+
+const bigNumber =  3423424343242342342423423423423
+//console.log(bigNumber)
+
+
+//Array 
+const  heros = ['kbw','uak','hh', 'ha'];
+let myObj = 
+{
+    name: "Hitesh",
+    age: 22,
+}
+
+//functions
+
+
+const myFunction = function() {
+
+    console.log("Hello World")
+
+}
+
+//Objects are always in curley braces
+// data type pata krna kis b value ka
+
+console.log(typeof bigNumber)
+
+
+
+
