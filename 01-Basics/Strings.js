f5b666f3383a1a1c17cce1a2197dfda5e6e70a98
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-12 08:56:23 +0000

    changed

--- 01-Basics/Strings.js
+++ 01-Basics/Strings.js
@@ -9,6 +9,10 @@ console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
 
 const gameName = new String('hiteshhc')
 //for accessing keys
-console.log(gameName[0]);
-console.log(gameName.__proto__);
-console.log(gameName.length);
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'))
