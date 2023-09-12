df9dff862f4996887933b429733fb16c245d83ff
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-12 06:58:04 +0000

    updated

--- 01-Basics/Strings.js
+++ 01-Basics/Strings.js
@@ -6,3 +6,9 @@ const repoCount = 50
 // use back tikcs string interpolation placeholder variable inject
 
 console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String('hiteshhc')
+//for accessing keys
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
