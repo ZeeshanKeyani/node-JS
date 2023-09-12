222ea48353367ccb4d0579bd7bcd215bfbbed75a
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-12 09:37:14 +0000

    2:40

--- 01-Basics/Strings.js
+++ 01-Basics/Strings.js
@@ -5,7 +5,7 @@ const repoCount = 50
 // this is the old method of 
 // use back tikcs string interpolation placeholder variable inject
 
-console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+//console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
 
 const gameName = new String('hiteshhc')
 //for accessing keys
@@ -14,5 +14,23 @@ const gameName = new String('hiteshhc')
 //console.log(gameName.length);
 //console.log(gameName.toUpperCase());
 
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'))
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('t'))
+
+const nameOne = new String ('zeeshank-hc')
+
+//console.log(nameOne.charAt(0));
+//console.log(nameOne.indexOf('z'));
+//console.log(nameOne.toLocaleLowerCase());
+//console.log(nameOne.toUpperCase());
+
+const newString = nameOne.substring(-8,4)
+console.log(newString);
+
+const anOtherString = nameOne.slice(-8,4)
+console.log(anOtherString)
+
+//trim and replace 
+const newStringOne = "       hiteash    "
+console.log(newStringOne);
+console.log(newStringOne.trim())
