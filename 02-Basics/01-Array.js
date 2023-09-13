2ef470733ec815ea3cd3c36dcca966ed8d3de624
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-13 19:24:05 +0000

    .

--- 02-Basics/01-Array.js
+++ 02-Basics/01-Array.js
@@ -1,17 +1,17 @@
 //arrays 
 // in javascript the arrays are resizeable 
-const myArr = [0,1,2,3,4,5]
-const myHeros = ["kbw", "ubk"]
+//const myArr = [0,1,2,3,4,5]
+//const myHeros = ["kbw", "ubk"]
 
-const myArr2 = new Array(1,2,3,4)
-console.log(myArr[1]);
+//const myArr2 = new Array(1,2,3,4)
+//console.log(myArr[1]);
 
 //array methods
 //myArr.push(6)
 //myArr.push(7)
 //myArr.push(6)
 //myArr.shift(9)
-myArr.shift()
+//myArr.shift()
 //console.log(myArr)
 //console.log(myArr.includes(3));
 //console.log(myArr.indexOf(3));
@@ -19,3 +19,9 @@ myArr.shift()
 //const newArray = myArr.join()
 //console.log(myArr);
 //console.log(newArray)
+// Methods of array 
+const myArr = [1,2,3,4,5]
+const myHeros = ("hkbw","hubk")
+const myArr2 = new Array(1,2,3,4)
+
+console.log(myArr[0]);
