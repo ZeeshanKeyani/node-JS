7129a570628d98eda2683f0b21326ddc71160a71
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-13 20:29:49 +0000

    ,

--- 02-Basics/01-Array.js
+++ 02-Basics/01-Array.js
@@ -24,4 +24,35 @@ const myArr = [1,2,3,4,5]
 const myHeros = ("hkbw","hubk")
 const myArr2 = new Array(1,2,3,4)
 
-console.log(myArr[0]);
+//console.log(myArr[1]);
+
+
+// Method in array
+//push add values in the aray
+
+//myArr.push(6)
+//myArr.push(7)
+//myArr.pop()
+//myArr.unshift(9)
+//myArr.shift()
+//console.log(myArr.includes(4))
+//console.log(myArr.indexOf(-19))
+
+//const newArray = myArr.join()
+
+//console.log(myArr)
+//console.log(typeof newArray)
+
+//SLICE , SPLICE 
+
+console.log ("A", myArr);
+ 
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B", myArr);
+
+
+const myn2 = myArr.splice(1,3)
+console.log("C", myArr);
+console.log(myn2)
---
+++ 02-Basics/02-Array2.js
@@ -0,0 +1,8 @@
+const  marvel_heros = ["Thor", "Ironman", "Spiderman"]
+const dc_heros = ["superman", "flashman", "batman"]
+
+marvel_heros.push(dc_heros)
+
+//console.log(marvel_heros[3][1]);
+
+//concat method 
