ee89fcbd64d6725d2f87c5840b48113c573bdc85
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-13 10:46:53 +0000

    .

---
+++ 02-Basics/01-Array.js
@@ -0,0 +1,13 @@
+//arrays 
+// in javascript the arrays are resizeable 
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["kbw", "ubk"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[1]);
+
+//array methods
+myArr.push(6)
+myArr.push(7)
+myArr.push(6)
+console.log(myArr)
