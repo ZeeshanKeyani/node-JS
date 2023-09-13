5058f1b0bd985cbc6eebb6898131fde1b8c35416
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-13 06:50:35 +0000

    update

---
+++ 01-Basics/07-Datesinjs.js
@@ -0,0 +1,13 @@
+let myDate = new Date()
+//console.log(myDate.toString())
+//console.log(myDate.toDateString())
+//console.log(myDate.toLocaleString())
+//console.log(typeof myDate)
+
+
+//let mCreatedDate = new Date(2023 , 0, 23, 5 ,3);
+//console.log(mCreatedDate.toDateString());
+//console.log(mCreatedDate.toLocaleString());
+
+let myNewCreatedDate = new Date ("2023-01-14")
+console.log(myNewCreatedDate.toLocaleString());
