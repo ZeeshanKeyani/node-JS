64ad64bd9fadb967671d8e4ac802aa1c61b34f07
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-13 11:02:43 +0000

    .

--- 02-Basics/01-Array.js
+++ 02-Basics/01-Array.js
@@ -7,7 +7,15 @@ const myArr2 = new Array(1,2,3,4)
 console.log(myArr[1]);
 
 //array methods
-myArr.push(6)
-myArr.push(7)
-myArr.push(6)
-console.log(myArr)
+//myArr.push(6)
+//myArr.push(7)
+//myArr.push(6)
+//myArr.shift(9)
+myArr.shift()
+//console.log(myArr)
+//console.log(myArr.includes(3));
+//console.log(myArr.indexOf(3));
+
+//const newArray = myArr.join()
+//console.log(myArr);
+//console.log(newArray)
