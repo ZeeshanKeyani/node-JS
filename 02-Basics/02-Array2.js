a79051e120ac1595383323dceea341d5302704fc
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-14 05:00:17 +0000

    .

--- 02-Basics/02-Array2.js
+++ 02-Basics/02-Array2.js
@@ -1,10 +1,48 @@
-const  marvel_heros = ["Thor", "Ironman", "Spiderman"]
-const dc_heros = ["superman", "flashman", "batman"]
+//const  marvel_heros = ["Thor", "Ironman", "Spiderman"]
+//const dc_heros = ["superman", "flashman", "batman"]
 
-marvel_heros.push(dc_heros)
+//marvel_heros.push(dc_heros)
 
 //console.log(marvel_heros[3][1]);
 
 //concat method 
-const allHeros = marvel_heros.concat(dc_heros)
-console.log(allHeros);
+//const allHeros = marvel_heros.concat(dc_heros)
+//console.log(allHeros);
+
+const marvel_heros = ["Thor", "Ironman", "Spiderman"]
+const dc_heros = ["superman","Flashman","Batman"]
+
+// push array in to array
+//marvel_heros.push(dc_heros)
+//console.log(marvel_heros);
+//console.log(marvel_heros[3][1]);
+
+//let allHeros = marvel_heros.concat(dc_heros)
+//console.log(allHeros);
+
+//we can also use spread
+
+const allHeros = [...marvel_heros, ...dc_heros]
+//console.log(allHeros);
+
+
+const anOtherArray = [1,2,3,[4,5,6],7, [6,7,[4,5]]]
+
+const real_another_array = anOtherArray.flat(Infinity)
+console.log(real_another_array)
+
+
+//array for the seperation of data 
+console.log(Array.isArray("Hitesh"))
+
+//to convert array isarray.from
+
+console.log(Array.from("Hitesh"))
+
+console.log(Array.from({name: "hitesh"}))  //intresting
+
+let score1 = 100
+let score2 =200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3))
