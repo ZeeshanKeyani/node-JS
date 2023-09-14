a8fd73fef10a1ffd3bd9de105ec58192bf21c2b5
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-14 06:39:05 +0000

    .

--- 02-Basics/03-Objects.js
+++ 02-Basics/03-Objects.js
@@ -4,11 +4,13 @@
 
 //object literals
 
-const mySym = Symbol ("Key")
+const mySym = Symbol ("Key1")
 
 const JsUser = {
 
     name: "Hitesh",
+    "full name": "Hitesh Chaoudhary",
+    [mySym]: "myKey1",
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
@@ -17,6 +19,21 @@ const JsUser = {
 
 }
 
-console.log(JsUser.name)
-console.log(JsUser["email"])
+//console.log(JsUser.name)
+//console.log(JsUser["email"])
+//console.log(JsUser["full name"])
+//console.log(typeof JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+Object.freeze(JsUser)
+JsUser.email="hitesh@microsoft.com"
+//console.log(JsUser)
+
+JsUser.greetings = function(){
+
+    console.log("Hello JsUser");
+}
+
+console.log(JsUser.greetings());
+
 
