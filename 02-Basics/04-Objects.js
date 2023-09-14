cd2cbb5862bc1cfd2c4d83aaa3379b5bd64d9e4f
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-14 07:57:02 +0000

    .

---
+++ 02-Basics/04-Objects.js
@@ -0,0 +1,24 @@
+//singlton constructor
+//const tinderUser = new  Object()
+//const tinderUser ={}
+
+//tinderUser.id = "123abc" 
+//tinderUser.name ="Sammy"
+//tinderUser.isLoggedIn = false
+
+//console.log(tinderUser)
+
+const regularUser = {
+
+    email: "some@gmail.com",
+    fullName: {
+        userFullName:{
+            firstName: "Hitesh",
+            lastName: "Chaoudhary"
+
+        }
+
+    }
+}
+
+console.log(regularUser.fullName);
