4cff1b5f59e039a0519ee793706be0fd50f88b8c
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-15 05:35:03 +0000

    .

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -14,9 +14,17 @@ function sayMyName(){
 
 //reference   execute
   //sayMyName()
+// parameters : function jub banatay hain us main jo bhi input laity hian iso ko parameters bolty hain 
+// arguments : jub fuction ko call kratay hain us time jo value us k andar pass krty hain us ko bolty hain arguments
 
   function addTwoNumaber(number1 , number2){
     console.log(number1 + number2);
   }
 
       addTwoNumaber(3,7)
+      addTwoNumaber(3, "7")
+      addTwoNumaber(3,"a")
+      // we can store a value of function  in the variable
+
+      const result = addTwoNumaber(1,3)
+    
