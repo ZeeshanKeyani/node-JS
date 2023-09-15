bd07133ed0275df3bab67fe491d7597b84ba989a
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-15 05:16:39 +0000

    .

---
+++ 03_Basics/01_Functions.js
@@ -0,0 +1,22 @@
+//jo bhi aap ny code likha hain usy apny aik package main
+// us pakage ki copy jitny bar utha k le jayn jahan jayen ly ja skty hain
+
+
+
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+//reference   execute
+  //sayMyName()
+
+  function addTwoNumaber(number1 , number2){
+    console.log(number1 + number2);
+  }
+
+      addTwoNumaber(3,7)
