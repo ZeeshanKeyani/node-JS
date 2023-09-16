b8aa40ccea08403db1822d3823e584447228cc65
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-16 14:18:37 +0000

    .

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -11,20 +11,39 @@ function sayMyName(){
     console.log("S");
     console.log("H");
 }
-
 //reference   execute
   //sayMyName()
 // parameters : function jub banatay hain us main jo bhi input laity hian iso ko parameters bolty hain 
 // arguments : jub fuction ko call kratay hain us time jo value us k andar pass krty hain us ko bolty hain arguments
 
+// function addTwoNumaber(number1 , number2){
+  //   console.log(number1 + number2);
+  // }
   function addTwoNumaber(number1 , number2){
-    console.log(number1 + number2);
-  }
-
-      addTwoNumaber(3,7)
-      addTwoNumaber(3, "7")
-      addTwoNumaber(3,"a")
-      // we can store a value of function  in the variable
-
-      const result = addTwoNumaber(1,3)
     
+    // let result = number1 + number2
+    // return result
+    
+    //will not display this below messat afer returnstatement
+    //console.log("Hitesh");
+    return number1 + number2
+  }
+      addTwoNumaber(3,5)
+      const result = addTwoNumaber(3,4)
+//      console.log("Result:" , result)
+
+function loginUserMessage(username)
+//username do yahan parameters main jo bhi argument pass kro gay usy main smjh lu ga aur usay values main add kr dunga
+  {
+    //if(username === undefined)   //ye conditoin to tub run ho ga jb hamari condition true main he evaluate ho 
+      if(!username)
+        {
+      console.log("Please enter a username")
+      return
+        }
+        return `${username} just loged in `
+}
+  // console.log(loginUserMessage("Hitesh"));
+ console.log(loginUserMessage());
+//agar kis ny is main arguments he ni pass kiay to kia out put aye ga
+//undefined
