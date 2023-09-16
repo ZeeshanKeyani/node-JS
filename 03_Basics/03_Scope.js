7a5ac68048bb47ab25936dba135c5c8bd7f641d9
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-16 18:41:12 +0000

    .

--- 03_Basics/03_Scope.js
+++ 03_Basics/03_Scope.js
@@ -10,9 +10,50 @@ let a =300;
 if(true){
     let    a = 10; 
     const  b = 20;
-    console.log("Inner", a);
+    //console.log("Inner", a);
   //  var    c  = 30;
         }
     //console.log(a);
     //console.log(b);
-    console.log(a);
+    //console.log(a);
+
+   // nested scope
+
+   function one() {
+    const username = "hitesh"
+
+    function two()
+    {
+        const website = "youtube"
+        console.log(username);
+    }
+   // console.log(website);
+    two()
+   }
+   //one()
+
+   if(true){
+
+        const username = "Hitesh"
+        if(username ==="Hitesh"){
+            const website = "youtube"
+            //console.log(username + website);
+        }
+        //console.log(website);
+   }
+            //console.log(username);
+
+    //++++++++++++++++++++++++++++++++++  Intresting ++++++++++++++++++++++++++++++++++++++//
+
+    console.log(addone(5));   
+function addone(num){
+
+    return num+1
+}
+
+addTwo(5)
+const addTwo = function(num){
+
+    return num + 2
+}
+
