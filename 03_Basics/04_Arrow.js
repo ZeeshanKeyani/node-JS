f4f0bbec708d5420680000c54f558c91043b5cb0
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-18 04:07:34 +0000

    .

--- 03_Basics/04_Arrow.js
+++ 03_Basics/04_Arrow.js
@@ -1,45 +1,58 @@
-const user ={
-    username: "hitesh",
-    price: 999,
-    prereq: "development",
+// const user ={
+//     username: "hitesh",
+//     price: 999,
+//     prereq: "development",
 
-    welcomeMessage: function (){
-            //this is used for the curren content for accessing
-        console.log(`${this.username}, Welcome to website`);
-        console.log(`${this.price}, your fee wil be `);
-        console.log(`${this.prereq}, will the prequiest for enterning this field`)
-        console.log(this)
-    }
-}
-        //user.welcomeMessage()
-        //user.username ="sam"
-        //user.welcomeMessage()
-        //console.log(this);
+//     welcomeMessage: function (){
+//             //this is used for the curren content for accessing
+//         console.log(`${this.username}, Welcome to website`);
+//         console.log(`${this.price}, your fee wil be `);
+//         console.log(`${this.prereq}, will the prequiest for enterning this field`)
+//         console.log(this)
+//     }
+// }
+//         //user.welcomeMessage()
+//         //user.username ="sam"
+//         //user.welcomeMessage()
+//         //console.log(this);
 
 
-        // function chai() {
-        //     let username ="Hitesh"
-        //     //console.log(this);
-        //     console.log(this.username)
-        // }
-        // chai()
+//         // function chai() {
+//         //     let username ="Hitesh"
+//         //     //console.log(this);
+//         //     console.log(this.username)
+//         // }
+//         // chai()
 
-       const chai =  () => {
-            let username ="Hitesh"
-            //console.log(this);
-            console.log(this);
-        }
-        //chai()
+//        const chai =  () => {
+//             let username ="Hitesh"
+//             //console.log(this);
+//             console.log(this);
+//         }
+//         //chai()
 
-        // const addTwo = (num1, num2) => {
-        //         return num1 + num2
+//         // const addTwo = (num1, num2) => {
+//         //         return num1 + num2
 
-        // }
-//implecet return
-//const addTwo = (num1, num2) => (num1 + num2)
-// agar object return kro gay to paranthesis man rap krna e paray ga
-const addTwo = (num1, num2) => ({username: "Hitesh"})
+//         // }
+// //implecet return
+// //const addTwo = (num1, num2) => (num1 + num2)
+// // agar object return kro gay to paranthesis man rap krna e paray ga
+// const addTwo = (num1, num2) => ({username: "Hitesh"})
         
-        console.log(addTwo(3,4));
+//         console.log(addTwo(3,4));
 
-// agar curly braces use hua   need to type return if pranthese curly brace lazmi
+// // agar curly braces use hua   need to type return if pranthese curly brace lazmi
+
+
+const user ={
+    username: "Hitesh",
+    price: 999,
+
+    welcomeMessage: function(){
+
+        
+    }
+
+
+}
