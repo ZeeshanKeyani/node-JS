0d8892ac21ea5ebf20fe271e3470b50ef73adfe5
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-20 09:49:01 +0000

    .

--- 04_ControlFlow/one.js
+++ 04_ControlFlow/one.js
@@ -1,15 +1,34 @@
 const isUserLogIn = true
+const temprature = 41
 
-if (2 == "2" ){
-    console.log("Executed");
+// if (temprature === 40 ) {
+//     console.log("less than 50");
+//     }
+//     else{
+//     console.log("temprature is greater than 50");
+//     }
+//     console.log("Execute");
+    
+// if (2 == "2" ){
+//     console.log("Executed");
 
-}
-// ==== also checks the data type of the 
-if (2 === "2" ){
-    console.log("Executed");
+// }
+// // ==== also checks the type cheking of the 
+// if (2 === "2" ){
+//     console.log("Executed");
 
-}
-if (2 != 3 ){
-    console.log("Executed3");
+// }
+// if (2 != 3 ){
+//     console.log("Executed3");
+// }
 
-}
+
+// const score = 200
+// // if we use const the issue of the sope is arises 
+// // if we use var then this issue will not arise
+// if (score > 100){
+//     var power ="fly"
+//     console.log(`User Power: ${power}`);
+// }
+// console.log(`User Power:  ${power}`);
+
