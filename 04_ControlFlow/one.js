f8d7df6298dda737867d1582825f739b1599ab34
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-20 22:14:33 +0000

    ,

--- 04_ControlFlow/one.js
+++ 04_ControlFlow/one.js
@@ -1,5 +1,5 @@
-const isUserLogIn = true
-const temprature = 41
+//const isUserLogIn = true
+//const temprature = 41
 
 // if (temprature === 40 ) {
 //     console.log("less than 50");
@@ -34,6 +34,60 @@ const temprature = 41
 
 //shorthand notation 
 
-const balance =1000
-if (balance > 5000) console.log("test"), console.log("test2");
+// const balance =1000
+// if (balance > 5000) console.log("test"), console.log("test2");
+// <,> <=, >=, =, ==, !=, ===, !==
+// if (2 != 3) {
+//     console.log("Executed")
+    
+// // }
+
+// const isUserLogIn = true
+// const temprature = 41
+
+// if (temprature === 40) {
+//     console.log("Temprature is less than 50");
+    
+// }else{
+// console.log("Temprature is greater than 50")}
+// console.log("Executed")
+
+// const score = 200
+// if (score >100) {
+//     let power = "fly"
+//     console.log(`The power of the user is ${power}`);
+    
+// }
+// console.log(`The power of the user is ${power}`);
+
+
+const balance = 1000
+
+//if (balance > 500) console.log("test"),console.log("test2");
+
+//nesting 
+// if (balance < 500){
+//     console.log("less than 500");
+// }else if (balance < 750) {
+//     console.log("less than 500");
+//  }else if (balance < 900) {
+//     console.log("less than 900");
+//  }
+//  else{
+//     console.log("less than 1200");
+//  }
+
+
+const isUserLogIn = true
+const debitCard = true
+const logedInFromGoogle= false
+const logedInFromEmail = true
+
+if (isUserLogIn && debitCard && 2==3){
+    console.log("Allow to buy course");
+}
+if (logedInFromGoogle || logedInFromEmail) {
+    console.log("user loggedIn");
+    
+}
 
