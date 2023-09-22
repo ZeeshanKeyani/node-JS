de3d743234b4382a64b445b8650bc666db2bceb4
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-22 21:36:38 +0000

    .

--- 04_ControlFlow/truthy.js
+++ 04_ControlFlow/truthy.js
@@ -31,8 +31,15 @@ if(Object.keys(emptyObject).length ===0){
 // nullish coalescing Operators (???): null undefined
 
 let val1;
-val1 = 5 ?? 10
-val1 = null ?? 10
-
+//val1 = 5 ?? 10
+//val1 = null ?? 10
+//val1 = undefined ?? 10
+val1 = null ?? 10 ?? 20
 
 console.log(val1)
+//Ternairy Operators
+
+//condition ? true : false
+
+const iceTeaPrice =100
+iceTeaPrice <= 80 ? console.log("less than 80 ") : console.log("greater")
