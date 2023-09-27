c1ad407b0d5febf6437aee1b1feaab89aff1c21e
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-27 20:41:33 +0000

    k

--- 05_Itrations/one.js
+++ 05_Itrations/one.js
@@ -1,5 +1,14 @@
 for (let i = 1; i <=10; i++) {
-    console.log(` Outer loop V: ${i}`);
+    //console.log(` Outer loop V: ${i}`);
  for (let j = 1; j <=10; j++) {
-    console.log(i +  '*' + j + '=' + i*j);
+    //console.log(i +  '*' + j + '=' + i*j);
 }}
+
+let myArray = ["flashman ","batman","superman"]
+console.log(myArray.length)
+for (let index = 0; index < myArray.length; index++)
+ {
+    const element = myArray[index];
+    console.log(element);
+    
+}
