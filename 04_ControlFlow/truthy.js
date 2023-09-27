d75376f5af6b17be1bfbc1053e084bc188b8618e
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-09-27 20:09:25 +0000

    ,

--- 04_ControlFlow/truthy.js
+++ 04_ControlFlow/truthy.js
@@ -1,45 +1,69 @@
 //if no comparison then the value will be considerd as the turth value
-//rather the value is empty in the strin then it will be assumed as the fals one 
+//rather the value is empty in the strin then it will be assumed as the fals one
 
-const userEmail = "h@hitesh.ai"
-//const userEmail = ""
+//const userEmail = "h@hitesh.ai"
+const userEmail = ""
 if (userEmail) {
-    console.log("GOt user email ");
+ //   console.log("GOt user email ");
 }else{
-    console.log("Don't have the user email.")
+   // console.log("Don't have the user email.")
 }
 
-//falsy values 
+//falsy values
 //fals, 0 , -0 BigInt 0, "" , null , undefined , NaN
 
-//truhy values 
+//truhy values
 // "0", 'false', "", {}, [], funciton(){}
-if (userEmail.length === 0){
-    console.log("Array is empty")
-}
+// if (userEmail.length === 0){
+//     console.log("Array is empty")
+// }
 
 //how to check empty object
-const emptyObject = {
-    user: "ali"
-}
-if(Object.keys(emptyObject).length ===0){
-    console.log("Object is empty")
-}else
-{
-    console.log("Object is  not empty")
-}
+// const emptyObject = {
+//     user: "ali"
+// }
+// if(Object.keys(emptyObject).length ===0){
+//     console.log("Object is empty")
+// }else
+// {
+//     console.log("Object is  not empty")
+// }
 // nullish coalescing Operators (???): null undefined
 
 let val1;
 //val1 = 5 ?? 10
 //val1 = null ?? 10
 //val1 = undefined ?? 10
-val1 = null ?? 10 ?? 20
+//val1 = null ?? 10 ?? 20
 
-console.log(val1)
+//console.log(val1)
 //Ternairy Operators
 
 //condition ? true : false
 
-const iceTeaPrice =100
-iceTeaPrice <= 80 ? console.log("less than 80 ") : console.log("greater")
+//const iceTeaPrice =100
+//iceTeaPrice <= 80 ? console.log("less than 80 ") : console.log("greater")
+
+//falsy values
+// fals, 0, -0, BigInt, On, "", null, undefined , NaN
+
+// truthy values
+// "0", 'false' "", [],{}, function(), { }
+
+if (userEmail.length === 0) {
+    console.log("Array is EMPTY")
+   
+}
+//we can check object as well in 
+const emptyObject ={}
+if (Object.keys(emptyObject).length===0) {
+    console.log("Object is Empty")
+    
+}
+
+
+//val1 = 5 ?? 10
+//val1 = null ?? 10
+varl1 = undefined ?? 15
+
+console.log(val1);
