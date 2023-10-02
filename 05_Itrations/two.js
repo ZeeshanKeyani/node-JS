660280aefc5116f496abd53e944f0cc00860b851
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-10-02 07:06:06 +0000

    .

--- 05_Itrations/two.js
+++ 05_Itrations/two.js
@@ -1,5 +1,10 @@
 for (let index = 0; index <= 10; index++) {
     const element = index;
     console.log(element)
+    if (element == 5) {
+        console.log("5 is the best ")
+        
+    }
+             console.log("element"); 
     
 }
