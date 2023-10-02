2aa64a1ed49834e8005d242d5d4a91325010b4cb
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-10-02 07:59:53 +0000

    .

--- 05_Itrations/two.js
+++ 05_Itrations/two.js
@@ -2,9 +2,19 @@ for (let i = 0; i <= 10; i++) {
     const element = i;
     
     if (element == 5) {
-        console.log("5 is the best ")
+       // console.log("5 is the best ")
         
     }
-             console.log("element"); 
+             //console.log("element"); 
     
 }
+
+for (let i = 0; i <=10; i++) {
+    //console.log(`Outer loop value: ${i}`)
+    for (let j = 0; j <=10; j++) {
+        //console.log(`Outer loop is: ${i} and Inner loop value: ${j}`)
+        //console.log(i+ '*' +j+ '=' +i*j );
+
+}    
+}
+    
