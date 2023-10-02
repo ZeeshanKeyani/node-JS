2a7378aee1802eb9839b1cd962d9a36f8e6cde81
ZeeshanKeyani <59744185+ZeeshanKeyani@users.noreply.github.com>
2023-10-02 09:07:39 +0000

    .

--- 05_Itrations/two.js
+++ 05_Itrations/two.js
@@ -16,5 +16,18 @@ for (let i = 0; i <=10; i++) {
         //console.log(i+ '*' +j+ '=' +i*j );
 
 }    
-}
-    
+} let myArray = ["1","2","3","4","5","6","7","8","9","10"]
+    //console.log(myArray.length);
+ for (let index = 0; index < myArray.length; index++){
+        const element = myArray[index];
+      //  console.log(element);
+ }
+
+    for (let index = 1; index <=20; index++) {
+        if (index == 5) {
+            console.log(" Detected 5");
+            break
+             }
+        console.log(`value of index is : ${index}`);
+        
+        }
