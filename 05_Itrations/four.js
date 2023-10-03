0c6134ee02e019c16e3ebe8d7dff2e6cdcbdb25b
ajay <ajay@123.com>
2023-10-03 23:43:55 +0500

    .

--- 05_Itrations/four.js
+++ 05_Itrations/four.js
@@ -11,8 +11,23 @@ const myObject ={
     }
     const programming = ['js','cp','rb','java','py']
     for (const key in programming) {
-            console.log(programming[key]); 
+           // console.log(programming[key]); 
             //this will run the requiered results
 
         }
     
+const programmingLang = ["java","c++","ruby","java","pythone"]
+        for (const key in programmingLang) {
+            //console.log(programmingLang[key]);
+                
+            }
+const map = new Map()
+map.set('PK','Pakistan')
+map.set('AF','Afgahanistan')
+map.set('AUS','Austrila')
+map.set('NZ','Newziland') 
+
+for (const [key, val] of map) {
+    console.log(key, ':-', val); 
+    
+}
