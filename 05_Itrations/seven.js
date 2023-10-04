6a9691559a98eeb20f63287bf34cb0503b92240b
ajay <ajay@123.com>
2023-10-05 02:21:55 +0500

    .

--- 05_Itrations/seven.js
+++ 05_Itrations/seven.js
@@ -8,6 +8,13 @@
 //                 .filter((num)=> num >= 40)                
 // console.log(newNums);
 
+const myArr = [1,2,3,4,5,6,7,8,9,10]
+
+const arrItr = myArr
+        .map((num) => num * 10)
+        .map((num) => num +1)
+        .filter((num) => num >= 40)
+console.log(arrItr);
 
 
 
