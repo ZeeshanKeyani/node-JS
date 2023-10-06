0a3f7100c3b7410b9440b1f1a920fcb1a8160f44
ajay <ajay@123.com>
2023-10-07 01:59:45 +0500

    .

--- 05_Itrations/nine.js
+++ 05_Itrations/nine.js
@@ -1,12 +1,12 @@
 //javascript map and reduce
 
-const myNums = [1,2,3]
+ const myNums = [1,2,3]
 
-//exemple in simple funcition 
-const myTotal = myNums.reduce(function (acc, currVal) {
-    //console.log(`Accumulate : ${acc} and  current value  : ${currVal}`)
-    return acc + currVal
-},0)
+// //exemple in simple funcition 
+// const myTotal = myNums.reduce(function (acc, currVal) {
+//     //console.log(`Accumulate : ${acc} and  current value  : ${currVal}`)
+//     return acc + currVal
+// },0)
 //console.log(myTotal);
 
 // const exnewNum = [1,2,3]
@@ -14,4 +14,23 @@ const myTotal = myNums.reduce(function (acc, currVal) {
 //     console.log(`Acct is : ${acc} and Current Value: ${current1}`)
 //     return acc +current1
 //     },0)
-//     console.log(practMyTotal);
+
+
+const myTotal = myNums.reduce( (acc, curr) => acc + curr, 0)
+console.log(myTotal);
+
+const shoppinCarr=[
+    
+{
+    itemName: "Js course ",
+    price:     299
+}, 
+
+{
+    itemName: " Mobile Devlopmment  ",
+    price:     5099
+}, 
+{
+    itemName: "Py course ",
+    price:     12999
+}]
